8b88bcb710b24b13a753535fc725d85c0418b382
naglismoc <dvd.filmas@gmail.com>
2022-10-25 11:43:10 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,55 @@
+console.log("yep yep! js failas!");
+
+console.log("labas rytas");
+const PI = 3.14;
+console.log(PI);
+// PI = 3.1428;
+console.log("O kas koliau?");
+let presidentsAge = 58;
+presidentsAge = 59;
+let whatWillHappen;
+console.log(whatWillHappen);
+whatWillHappen = 76;
+console.log(whatWillHappen);
+whatWillHappen = "Kestas";
+console.log(whatWillHappen);
+
+
+console.log(7 + 6 + " yra skaicius ");
+console.log(" yra skaicius " + 7 + 6);
+console.log(" yra skaicius " + (7 + 6));
+
+let isRed = true;
+let hasPorche = false;
+console.log("ar Vaidas turi porche? " + hasPorche);
+
+if (hasPorche) {
+    console.log("E davai pravesk pro rajona?");
+}
+
+console.log("a cia po to");
+
+if (isRed) {
+    console.log("Labas, Diana.");
+    console.log("Mačiau ieško raudonplaukių aktorių reklamai, gal nori sudalyvaut?");
+} else {
+    console.log("Labas, Diana, Gal pažįsti raudonplaukių merginų?");
+}
+
+console.log(Math.round(5.45));
+console.log(Math.round(5.5));
+console.log(Math.floor(5.9));
+console.log(Math.ceil(5.28));
+
+console.log(Math.round(Math.random() * 10));
+console.log(Math.round(Math.random() * 10));
+console.log(Math.round(Math.random() * 10));
+console.log(Math.round(Math.random() * 10));
+
+let rndNum = Math.round(Math.random() * 10);
+console.log(rndNum);
+console.log(rndNum);
+console.log(rndNum);
+console.log(rndNum);
+console.log(rndNum);
+console.log(rndNum);
